63be0ab6c45d461c2730c85b8f3790af2aa6dc2c
Nina <140155579+BeimnetTadesse@users.noreply.github.com>
2025-08-20 00:28:03 +0300

    Added categories, transactions, and budgets API endpoints in the core app

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Transaction, Budget, SavingsGoal, RecurringTransactionTemplate, Category
 
-# Register your models here.
+admin.site.register(Transaction)
+admin.site.register(Budget)
+admin.site.register(SavingsGoal)
+admin.site.register(RecurringTransactionTemplate)
+admin.site.register(Category)
